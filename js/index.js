facf7e9ce93a3d8d68da28ba92860aafbda41a09
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-08 10:01:47 -0500

    Creacion del preloader

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//TODOS Preloader
+var carga = document.getElementById("preloader");
+setTimeout(function() {
+    carga.style.opacity="1.0";
+    carga.style.opacity="0.75";
+    carga.style.opacity="0.5";
+    carga.style.opacity="0";
+    carga.style.visibility="hidden";
+},2150);
