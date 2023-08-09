0df1489ab4ee2584124276ab5d95091e29fe187a
MatiasCV27 <matiascriollo57@gmail.com>
2023-08-08 23:46:27 -0500

    Agregando los testimonios

--- js/index.js
+++ js/index.js
@@ -7,3 +7,25 @@ setTimeout(function() {
     carga.style.opacity="0";
     carga.style.visibility="hidden";
 },2150);
+//TODOS Preguntas
+window.sr = ScrollReveal(),
+sr.reveal('#logo', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+sr.reveal('.navbar-nav', {
+duration: 1000,
+origin: "left",
+distance: "500px"
+});
+sr.reveal('section', {
+duration: 1750,
+origin: "top",
+distance: "500px"
+});
+sr.reveal('main', {
+duration: 1750,
+origin: "bottom",
+distance: "500px"
+});
